1bc33ac581562a418172c568f5da411e46b8e24f
Greisy Marchena <Greisymarchena22@gmail.com>
2020-04-12 17:03:52 -0400

    Creacion de un controlador nuevo para las consultas y sus vistas tambien creadas.

---
+++ Proyectof/Proyectof/Controllers/ConsultasController.cs
@@ -0,0 +1,44 @@
+﻿using Proyectof.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyectof.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        private HospitalEntities db = new HospitalEntities();
+        // GET: Consultas
+        public ActionResult Pacientes()
+        {
+            return View(db.Pacientes.ToList());
+        }
+        public ActionResult Medicos()
+        {
+            return View(db.Medicos.ToList());
+        }
+        public ActionResult Habitaciones()
+        {
+            var habitaciones = db.Habitaciones.Include(h => h.Tipos);
+            return View(habitaciones.ToList());
+        }
+        public ActionResult Citas()
+        {
+            var citas = db.Citas.Include(c => c.Medicos).Include(c => c.Pacientes);
+            return View(citas.ToList());
+        }
+        public ActionResult Ingresos()
+        {
+            var ingresos = db.Ingresos.Include(i => i.Habitaciones).Include(i => i.Pacientes);
+            return View(ingresos.ToList());
+        }
+        public ActionResult AltaMedica()
+        {
+            var altaMedica = db.AltaMedica.Include(a => a.Habitaciones).Include(a => a.Ingresos).Include(a => a.Pacientes);
+            return View(altaMedica.ToList());
+        }
+    }
+}
